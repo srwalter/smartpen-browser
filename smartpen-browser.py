f3a6ad6f0bb6b0b98e600ec26cca91dba2480bc7
Steven Walter <stevenrwalter@gmail.com>
2010-01-28 23:18:41 -0500

    Drawing strokes on-screen from pre-fetched data

--- smartpen-browser.py
+++ smartpen-browser.py
@@ -3,17 +3,85 @@
 import gtk
 import pysmartpen
 import xml.dom.minidom
+import zipfile
+import parsestf
+
+class STFWidget(gtk.DrawingArea):
+    class Parser(parsestf.STFParser):
+        def __init__(self, stream):
+            super(STFWidget.Parser, self).__init__(stream)
+            self.last_force=0
+
+        def handle_stroke_end(self, time):
+            self.ctx.stroke()
+            self.last_force = 0
+
+        def handle_point(self, x, y, f, time):
+            ctx = self.ctx
+            if f:
+                if self.last_force:
+                    ctx.line_to(x/10, y/10)
+                else:
+                    ctx.move_to(x/10, y/10)
+            self.last_force = 1
+
+        def parse(self, ctx):
+            self.ctx = ctx
+            super(STFWidget.Parser, self).parse()
+
+    def __init__(self, *args):
+        super(STFWidget, self).__init__(*args)
+        self.connect('expose-event', self.expose)
+        self.parser = None
+        self.ctx = None
+
+    def expose(self, *args):
+        print "expose"
+        window = self.get_window()
+        ctx = window.cairo_create()
+        ctx.set_source_rgb(255, 255, 255)
+        ctx.paint()
+        ctx.set_source_rgb(0, 0, 0)
+        if self.parser is not None:
+            self.parser.parse(ctx)
+
+    def parse(self, stream):
+        p = STFWidget.Parser(stream)
+        self.parser = p
+
 
 class Notebook(object):
-    def __init__(self, notebook, guid, title):
+    def __init__(self, pen, guid, title, pages):
         self.guid = guid
         self.title = title
+        self.pen = pen
+        self.pages = pages
 
-        child = gtk.Label(guid)
+        child = STFWidget()
+        child.set_size_request(500, 500)
         child.show()
-        tab = gtk.Label(title)
+        self.contents = child
+
+    def add(self, notebook):
+        tab = gtk.Label(self.title)
         tab.show()
-        notebook.append_page(child, tab)
+        notebook.append_page(self.contents, tab)
+
+    def render(self):
+        #tmpfile = "tmpfile"
+        #self.pen.get_guid(tmpfile, self.guid)
+        tmpfile = file("/home/srwalter/programs/livescribe/data")
+        z = zipfile.ZipFile(tmpfile, "r")
+
+        name = None
+        addr = self.pages[0]
+        for fn in z.namelist():
+            #if addr in fn:
+            if fn.startswith('data/'):
+                name = fn
+                break
+        f = z.open(name)
+        self.contents.parse(f)
 
 class SmartpenBrowser(object):
     def pen_connect(self, *args):
@@ -26,6 +94,7 @@ class SmartpenBrowser(object):
 
         notebooks = []
         tabs = self.builder.get_object('notebook1')
+        tabs.connect('switch-page', self.switch_page, notebooks)
 
         for elm in cl.getElementsByTagName('lsp'):
             guid = elm.getAttribute('guid')
@@ -34,9 +103,20 @@ class SmartpenBrowser(object):
             print guid
 
             title = elm.getAttribute('title')
-            nb = Notebook(tabs, guid, title)
+
+            pages = []
+            for p in elm.getElementsByTagName('page'):
+                addr = p.getAttribute('pageaddress')
+                pages.insert(0, addr)
+
+            nb = Notebook(self.pen, guid, title, pages)
             notebooks.append(nb)
-        print notebooks
+            nb.add(tabs)
+        pass
+
+    def switch_page(self, notebook, page, page_num, notebooks):
+        print "switch page %d" % page_num
+        notebooks[page_num].render()
 
     def pen_disconnect(self, *args):
         self.pen.disconnect()
